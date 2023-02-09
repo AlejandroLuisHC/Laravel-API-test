3501f66a8fef22e19c4bc023ddf6820ddee6daf5
AlejandroLHC <alejandrolhc@gmail.com>
2023-02-09 20:47:05 +0100

    First commit: Create DataBase

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Customer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Customer>
+ */
+class CustomerFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Customer::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $type = $this->faker->randomElement(['Individual', 'Company']);
+        $name = $type === 'Individual' ? $this->faker->name() : $this->faker->company();
+
+        return [
+            'name' => $name,
+            'type' => $type,
+            'email' => $this->faker->unique()->safeEmail(),
+            'phone' => $this->faker->phoneNumber(),
+            'address' => $this->faker->address(),
+            'city' => $this->faker->city(),
+            'state' => $this->faker->state(),
+            'country' => $this->faker->country(),
+            'zip' => $this->faker->postcode(),
+        ];
+    }
+}
---
+++ database/factories/InvoiceFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Invoice;
+use App\Models\Customer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Invoice>
+ */
+class InvoiceFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Invoice::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $status = $this->faker->randomElement(['B', 'P', 'V']);
+
+        return [
+           'customer_id' => Customer::factory(),
+           'amount' => $this->faker->randomFloat(100, 20000),
+           'status' => $status,
+           'billed_at' => $this->faker->dateTimeThisDecade(),
+           'paid_at' => $status === 'P' ? $this->faker->dateTimeThisDecade() : null,
+        ];
+    }
+}
